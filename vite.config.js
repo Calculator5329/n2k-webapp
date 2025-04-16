f19c297c49b804e0c9cd19d6efdb91021b7751d0
Ethan <et2boys@hotmail.com>
2025-04-16 18:22:06 -0500

    Fix bugs 2.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/n2k-webapp",
+  base: "/n2k-webapp/",
   server: {
     proxy: {
       "/api": "https://n2k-backend-production.up.railway.app",
