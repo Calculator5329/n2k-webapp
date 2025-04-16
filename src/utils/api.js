24e776b11096da487486d5dc4589b664b8fd1d77
Ethan <et2boys@hotmail.com>
2025-04-16 18:07:06 -0500

    Update build

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import { auth } from "../firebase";
 
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://n2k-backend-production.up.railway.app/";
 
 async function getAuthToken() {
   const user = auth.currentUser;
